62e6c0200ca4caee0071f0a13a81dda1e7a78117
rock-tran <rock.tran@manadr.com>
2022-04-22 16:34:26 +0700

    Test

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Sketch, Page, Artboard } = require('sketch-constructor');
 
-path = process.cwd()
+const path = process.cwd()
 
-// Sketch.fromFile(`${process.cwd()}`)
+Sketch.fromFile(`${process.cwd()}/files/Rock 2.sketch`).then((sketch) => {
+    const artboard = sketch.getPages()[1].getArtboards()[0]
+    const layers = artboard.layers
+    console.log("ABC");
+
+});
 
 console.log("ToanTK")
